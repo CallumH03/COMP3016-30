166b4a81d54768b1d24653dbe5d7d7542b72fead
CallumH03 <callum.hill-16@students.plymouth.ac.uk>
2023-11-14 16:52:03 +0000

    Integrated maze levels
    Now goes to maze 2 after maze 1 completion

--- COMP3016-30/COMP3016-30/COMP3016-30.cpp
+++ COMP3016-30/COMP3016-30/COMP3016-30.cpp
@@ -10,7 +10,7 @@ void displaymenu() {
     std::cout << "You are represented by 'P' on the maze." << std::endl;
     std::cout << "Use W/A/S/D to move (Up/Left/Down/Right)." << std::endl;
     std::cout << "Try to find your way to the key ('K') to open the door ('D')" << std::endl;
-    std::cout << "You can not go through walls ('#')" << std::endl;
+    std::cout << "You cannot go through walls ('#')" << std::endl;
     std::cout << "Type 'Q' to quit the game at any time." << std::endl;
     std::cout << "Type 'Play' to start" << std::endl;
 }
@@ -48,50 +48,22 @@ void clearConsole() {
 #endif
 }
 
-int main() {
-    std::string file_path = "Maze.txt";
+bool loadMaze(const std::string& file_path, std::vector<std::string>& maze, int& keyRow, int& keyCol, int& doorRow, int& doorCol) {
     std::ifstream file(file_path);
 
     if (!file.is_open()) {
         std::cerr << "Error opening file " << file_path << std::endl;
-        return 1;
+        return false;
     }
 
-    std::vector<std::string> maze;
-    std::string line;
+    maze.clear();
 
+    std::string line;
     while (std::getline(file, line)) {
         maze.push_back(line);
     }
 
     file.close();
-
-    std::string startCommand;
-    displaymenu();
-
-    while (true) {
-        std::cin >> startCommand;
-
-        std::transform(startCommand.begin(), startCommand.end(), startCommand.begin(), ::tolower);
-
-        if (startCommand == "play") {
-            break;
-        }
-        else if (startCommand == "quit" || startCommand == "q") {
-            std::cout << "Successfully quit the game" << std::endl;
-            return 0;
-        }
-        else {
-            std::cout << "Invalid command. Type 'play' to start or 'quit' to exit: ";
-        }
-    }
-
-    int playerRow, playerCol;
-    findPlayerPosition(maze, playerRow, playerCol);
-
-    int keyRow = -1, keyCol = -1;
-    int doorRow{}, doorCol{};
-    bool hasKey = false;
 
     for (int i = 0; i < maze.size(); ++i) {
         for (int j = 0; j < maze[i].size(); ++j) {
@@ -103,6 +75,44 @@ int main() {
                 keyRow = i;
                 keyCol = j;
             }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int currentLevel = 1;
+    std::vector<std::string> maze;
+    int playerRow, playerCol;
+    int keyRow, keyCol;
+    int doorRow, doorCol;
+    bool hasKey = false;
+
+    std::string startCommand;
+    displaymenu();
+
+    while (true) {
+        std::cin >> startCommand;
+
+        std::transform(startCommand.begin(), startCommand.end(), startCommand.begin(), ::tolower);
+
+        if (startCommand == "play") {
+            std::string file_path = "Maze" + std::to_string(currentLevel) + ".txt";
+            if (!loadMaze(file_path, maze, keyRow, keyCol, doorRow, doorCol)) {
+                std::cerr << "Error loading maze for level " << currentLevel << std::endl;
+                return 1;
+            }
+
+            findPlayerPosition(maze, playerRow, playerCol);
+            break;
+        }
+        else if (startCommand == "quit" || startCommand == "q") {
+            std::cout << "Successfully quit the game" << std::endl;
+            return 0;
+        }
+        else {
+            std::cout << "Invalid command. Type 'play' to start or 'quit' to exit: ";
         }
     }
 
@@ -169,8 +179,17 @@ int main() {
         }
 
         if (hasKey && playerRow == doorRow && playerCol == doorCol) {
-            std::cout << "Congratulations! You opened the door and completed the level!" << std::endl;
-            return 0;
+            std::cout << "Congratulations! You opened the door and completed level " << currentLevel << "!" << std::endl;
+            ++currentLevel;
+
+            std::string nextLevelPath = "Maze" + std::to_string(currentLevel) + ".txt";
+            if (!loadMaze(nextLevelPath, maze, keyRow, keyCol, doorRow, doorCol)) {
+                std::cerr << "Error loading maze for level " << currentLevel << std::endl;
+                return 1;
+            }
+
+            findPlayerPosition(maze, playerRow, playerCol);
+            hasKey = false;
         }
     }
 
